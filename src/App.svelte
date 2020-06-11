b3e4e851b7cb4e5336da41508956b179454a214d
Nick Manning <nickmanning214@gmail.com>
2020-06-11 07:32:45 -0400

    fix bugs

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,14 @@
 <script>
 	import Crud from './Crud.svelte';
 	let arr = ['One','Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten','Eleven','Twelve'];
-
+	function onChange(e){
+		console.log('on change')
+		arr = e.detail.arr;
+	}
 </script>
 
-<Crud arr={arr}></Crud>
+<Crud arr={arr} on:change={onChange}></Crud>
+
+<div>
+	[{arr}]
+</div>
--- src/Crud.svelte
+++ src/Crud.svelte
@@ -1,4 +1,8 @@
 <script>
+
+	import {createEventDispatcher} from 'svelte';
+	const dispatch = createEventDispatcher();
+	
 	import DraggableList from '@nickmanning214/svelte-draggable-list/src/DraggableList.svelte';
 	
 	import SpanEditable from '@nickmanning214/svelte-span-editable/src/SpanEditable.svelte';
@@ -6,31 +10,27 @@
 	import pullAt from 'lodash.pullat';
 
     let onEditInfo;
-    export let arr;
+	export let arr;
+	
+	
 	function onEdit(item,index,e){
 		arr[index] = e.detail.value
 		arr = arr;
+		dispatch('change',{arr})
 	}
 	
-	let currentDraggedIndex = null
-	let from;
-	let to;
-	function onMouseDown(e){
-		currentDraggedIndex = e.detail.i;
-	}
-	function onChange(e){
-		console.log("works")
-		arr = e.detail.newData;
-	}
+
+
 	
 	function del(index){
-		console.log('del')
 		pullAt(arr,index);
 		arr=arr;
+		dispatch('change',{arr})
 	}
 	
 	function add(){
 		arr = [...arr,'New'];
+		dispatch('change',{arr})
 	}
 
 	let isDragging = false;
@@ -42,23 +42,27 @@
 	function onFinishDrag(e){
 		isDragging = false;
 		arr = e.detail.data;
+		dispatch('change',{arr})
 	}
 	
 </script>
 
 <height>
-<DraggableList data={arr} let:item let:index on:mousedown={onMouseDown} on:change={onChange} on:startDrag={onStartDrag} on:finishDrag={onFinishDrag}>
-	<SpanEditable on:edited={onEdit.bind(null,item,index)} disabled={isDragging}>{item}</SpanEditable>
-	<span on:click={()=>{del(index)}}>[Del]</span>
-</DraggableList>
+	<DraggableList data={arr} let:item let:index on:startDrag={onStartDrag} on:finishDrag={onFinishDrag}>
+		<SpanEditable on:edited={onEdit.bind(null,item,index)} disabled={isDragging}>{item}</SpanEditable>
+		<span on:click={()=>{del(index)}}>[Del]</span>
+	</DraggableList>
 </height>
 <button on:click={add}>
 	Add
 </button>
-<div>
-[{arr}]
-currentChangeIndex: {from} {to}
-</div>
+
+
+
+
+
+
+
 <style>
 height{
 	display:block;
